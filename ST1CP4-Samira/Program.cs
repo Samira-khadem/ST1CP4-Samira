fd83e7b67e47b012570078258c7f2a10f27be647
samira khadem <samira.khademhoseini@gmail.com>
2020-10-30 10:28:00 +0100

    Add project files.

---
+++ ST1CP4-Samira/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ST1CP4_Samira
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
