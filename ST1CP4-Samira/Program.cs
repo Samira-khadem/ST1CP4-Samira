7db3fc299e8c4b5947afb2e2e21f1d502f0532de
samira khadem <samira.khademhoseini@gmail.com>
2020-10-30 17:21:12 +0100

    my new commit

--- ST1CP4-Samira/Program.cs
+++ ST1CP4-Samira/Program.cs
@@ -16,6 +16,7 @@ namespace ST1CP4_Samira
         {
             Class1 myClass = new ST1CP4_Samira.Class1();
             myClass.first();
+            
            // Console.WriteLine("Thank you for ordering." + myName + " here is details to your order: a " + pizza + "size" + size + "would be delivered at: " + ordTim + "at " + city + " " + street + " " + house);
 
 
